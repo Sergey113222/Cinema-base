80015da575ddf314f020aa4d22921ead4c935034
User <k-grine@mail.ru>
2021-04-18 13:10:22 +0300

    1.Create structure 2.Add maven dependencies

---
+++ src/main/java/info/movietrash/cinemabase/CinemaBaseApplication.java
@@ -0,0 +1,13 @@
+package info.movietrash.cinemabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/movietrash/cinemabase/CinemaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package info.movietrash.cinemabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
