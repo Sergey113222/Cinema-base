17599467cfc3bcf901eb5d6b2e00008472addc6f
Denisyasyuchenya <yasyuchenya1@gmail.com>
2021-05-13 18:18:36 +0300

    Mov-15-16. Please tell if I should add sth

--- src/main/java/info/movietrash/cinemabase/model/Profile.java
+++ src/main/java/info/movietrash/cinemabase/model/Profile.java
@@ -1,6 +1,7 @@
 package info.movietrash.cinemabase.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Entity
@@ -10,20 +11,34 @@ public class Profile extends BaseModel {
     @Column(name = "avatar")
     private String avatar;
     @Column(name = "about")
+    @Size(min = 10, max = 200, message
+            = "About must be between 10 and 200 characters")
     private String about;
     @Column (name = "email")
+    @Email(message = "Email should be valid")
+    @Pattern(regexp=".+@.+\\.[a-z]+")
     private String email;
     @Column(name = "first_name")
+    @NotNull(message = "First name cannot be null")
+    @Pattern(regexp = "[a-zA-Z]")
     private String firstName;
     @Column(name = "last_name")
+    @NotNull(message = "Last name cannot be null")
+    @Pattern(regexp = "[a-zA-Z]")
     private String lastName;
     @Column(name = "age")
+    @NotNull
+    @Min(value = 16, message = "Age should not be less than 16")
+    @Max(value = 99, message = "Age should not be greater than 99")
     private Integer age;
     @Column(name = "gender")
+    @NotNull
     private String gender;
     @Column(name = "region")
+    @NotNull
     private String region;
     @Column(name = "language")
+    @NotNull
     private String language;
 
     @OneToOne(mappedBy = "profile")
--- src/main/java/info/movietrash/cinemabase/model/User.java
+++ src/main/java/info/movietrash/cinemabase/model/User.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
@@ -12,22 +16,28 @@ public class User extends BaseModel {
     @Column(name = "username")
     @Getter
     @Setter
+    @NotNull(message = "Name cannot be null")
     private String username;
 
     @Column(name = "password")
     @Getter
     @Setter
+    @NotEmpty
+    @NotNull
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
 
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     @Getter
     @Setter
+    @NotNull
     private Role role;
 
     @Column(name = "active")
     @Getter
     @Setter
+    @AssertTrue
     private Boolean active = true;
 
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
