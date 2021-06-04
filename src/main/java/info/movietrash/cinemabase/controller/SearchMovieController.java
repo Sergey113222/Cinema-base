d22c78d8a70e8ec97f767f73e9bd3f12ffa1f415
Gritsuk_Sergey <k-grine@mail.ru>
2021-06-04 16:15:26 +0300

    add logger 1. fix application.properties 2. create writing logs to file

--- src/main/java/info/movietrash/cinemabase/controller/SearchMovieController.java
+++ src/main/java/info/movietrash/cinemabase/controller/SearchMovieController.java
@@ -5,6 +5,10 @@ import info.movietrash.cinemabase.dto.MovieDto;
 import info.movietrash.cinemabase.dto.SearchDto;
 import info.movietrash.cinemabase.service.SearchService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +19,6 @@ import java.util.List;
 public class SearchMovieController {
 
     private final SearchService searchService;
-
     @PostMapping
     public List<MovieDto> searchMoviesByName(@RequestBody SearchDto dto) throws JsonProcessingException {
         return searchService.searchMoviesByName(dto);
