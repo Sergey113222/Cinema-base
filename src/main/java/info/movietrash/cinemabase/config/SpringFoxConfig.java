311b86115a1d354c25bbe11266aeab27decadb54
Gritsuk_Sergey <k-grine@mail.ru>
2021-08-03 15:46:30 +0300

    03.08.21 beauty Swagger-UI

--- src/main/java/info/movietrash/cinemabase/config/SpringFoxConfig.java
+++ src/main/java/info/movietrash/cinemabase/config/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package info.movietrash.cinemabase.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -42,11 +43,12 @@ public class SpringFoxConfig {
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
-    private ApiInfo apiInfo(){
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Cinema-base Rest Application")
                 .description("App connects to the API of https://www.themoviedb.org \n" +
--- src/main/java/info/movietrash/cinemabase/controller/UserController.java
+++ src/main/java/info/movietrash/cinemabase/controller/UserController.java
@@ -2,6 +2,8 @@ package info.movietrash.cinemabase.controller;
 
 import info.movietrash.cinemabase.dto.UserDto;
 import info.movietrash.cinemabase.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +14,20 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/v1/users")
+@Api(tags = "controller-only-for-ADMIN")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
     @RolesAllowed("ADMIN")
+    @ApiOperation(value = "Find User by id")
     public UserDto findUserById(@PathVariable("id") Long id) {
         return userService.findUserById(id);
     }
 
     @GetMapping(value = "/param")
     @RolesAllowed("ADMIN")
+    @ApiOperation(value = "Find all sorted Users (choose type of sort(ASC/DESC) and by which column to sort)")
     public List<UserDto> findAllUsersSorted(
             @RequestParam Sort.Direction direction,
             @RequestParam String sortColumn) {
@@ -31,6 +36,7 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     @RolesAllowed("ADMIN")
+    @ApiOperation(value = "Delete User by id")
     public void deleteUserById(@PathVariable("id") Long id) {
         userService.deleteUser(id);
     }
